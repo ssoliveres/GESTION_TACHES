f707ef8608c55568f2c78c1e273633945a5136fb
stf <stephane.soliveres@viacesi.fr>
2017-01-17 22:07:01 +0100

    Mise à jour de README.md et de la classe Sql

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # GESTION_TACHES
+Gérer les tâches de tous les jours étalées sur l'année avec remonté automatique
+des tâches au fur et à mesure que le temps approche.
+
+La base utilisée dans la version initiale est Sqlite3.
--- Sql.py
+++ Sql.py
@@ -4,42 +4,39 @@
 import sqlite3
 
 class Sql:
-    def __init__(self):
-        self._fichier = "./BDD/taches.sq3"
-        _set_connexion_sqlite3()
+   def __init__(self):
+      self._fichier = "./BDD/taches.sq3"
+      _set_connexion_sqlite3()
 
-    def _set_connexion_sqlite3():
-        self._connexion = sqlite3.connect(self._fichier)
+   def _set_connexion_sqlite3():
+      self._connexion = sqlite3.connect(self._fichier)
 
-    def SQL_Disconnect():
-        self._connexion.close
+   def SQL_Disconnect():
+      self._connexion.close
 
-    def _get_fichier(self):
-        return self._fichier
+   def _get_fichier(self):
+      return self._fichier
 
-    def _set_fichier(self, file):
-        self._fichier = file
+   def _set_fichier(self, file):
+      self._fichier = file
 
-    def _set_curseur(self):
-        self._cursor = self._connexion.cursor()
+   def _set_curseur(self):
+      self._cursor = self._connexion.cursor()
 
-    def close_cursor():
-        self._cursor.close()
+   def close_cursor():
+      self._cursor.close()
 
-    def delete_table(t):
-        self._cursor.execute("DROP TABLE " + t + ";")
-        
-#fichierBDD = "./BDD/taches.sq3"
-#connexion = sqlite3.connect(fichierBDD)
-#cursor = connexion.cursor()
+   def delete_table(t):
+      self._cursor.execute("DROP TABLE " + t + ";")
+
+   def show_table(t):
+      self._cursor.execute("SELECT * FROM " + t)
+      for taskList in self._cursor:
+         print tasklist
+   
 cursor.execute("CREATE TABLE IF NOT EXIST taches (id INTEGER, nom TEXT, debut DATE, priorite INT)")
 
 cursor.execute("INSERT INTO taches(id, nom, debut, priorite) VALUES(1,'Premiere tache',20170112,1)")
 cursor.execute("INSERT INTO taches(id, nom, debut, priorite) VALUES(2,'Deuxieme tache',20170112,1)")
 
-cursor.execute("SELECT * FROM taches")
-for task in cur:
-    print(task)
 connexion.commit()
-#cursor.close()
-#connexion.close()
